aa1adc29d3656dd336d1c07921ddc6a395608129
Mridul Shrivastava <mridul.shrivastava@pnp-hcl.com>
2024-09-10 14:24:18 +0530

    Added nose ring

--- src/util/demo-map.ts
+++ src/util/demo-map.ts
@@ -2,6 +2,10 @@ import sapphireEarringThumb from '../static/assets/thumbnail-sapphire-earring.pn
 import sapphireEarring from '../static/assets/3D-sapphire-earring.glb';
 import rubyDiamondThumb from '../static/assets/thumbnail-ruby-diamond-necklace.png';
 import rubyDiamondNecklace from '../static/assets/3D-ruby-dia-necklace.glb';
+import demoGoldNoseRingThumb from '../static/assets/thumbnail-gold-nosering.png';
+import demoGoldNoseRing from '../static/assets/3D-gold-nosering.glb';
+// import demoGoldNoseRingThumb from '../static/assets/nose.png';
+// import demoGoldNoseRing from '../static/assets/nose.glb';
 import { IModel } from '../types/types';
 
 export const models: IModel[] = [
@@ -16,7 +20,7 @@ export const models: IModel[] = [
     entityName: 'sapphireEarring-entity',
     modelId: 'sapphireEarringModel',
     modelSrc: sapphireEarring,
-    selected: true,
+    selected: false,
   },
   {
     name: 'sapphireEarring',
@@ -29,7 +33,7 @@ export const models: IModel[] = [
     entityName: 'sapphireEarring-entity',
     modelId: 'sapphireEarringModel',
     modelSrc: sapphireEarring,
-    selected: true,
+    selected: false,
   },
   {
     name: 'rubyDiamondNecklace',
@@ -43,5 +47,18 @@ export const models: IModel[] = [
     modelId: 'rubyDiamondNecklaceModel',
     modelSrc: rubyDiamondNecklace,
     selected: false,
+  },
+  {
+    name: 'demoGoldNoseRing',
+    type: 'nosering',
+    thumbnailSrc: demoGoldNoseRingThumb,
+    anchorIndex: 363,
+    rotation: '0.2 130 20',
+    position: '0 0 0',
+    scale: '0.005 0.005 0.005',
+    entityName: 'demoGoldNoseRing-entity',
+    modelId: 'demoGoldNoseRingModel',
+    modelSrc: demoGoldNoseRing,
+    selected: true,
   }
 ];
